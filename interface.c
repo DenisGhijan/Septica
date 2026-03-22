1325eb4f83973811bd558b01f4b03ca95c1f9485
DenisGhijan <denis-mihail-adrian.ghijan@student.upt.ro>
2026-03-22 11:32:22 +0200

    interface.c added ascii art for cards

--- interface.c
+++ interface.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "interface.h"
- 
+
+// --- OS-SPECIFIC SETUP ---
 #ifdef _WIN32
     #include <conio.h>
 #else
@@ -12,7 +13,7 @@
         int ch;
         tcgetattr(STDIN_FILENO, &oldt);
         newt = oldt;
-        newt.c_lflag &= ~(ICANON | ECHO); 
+        newt.c_lflag &= ~(ICANON | ECHO);
         tcsetattr(STDIN_FILENO, TCSANOW, &newt);
         ch = getchar();
         tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
@@ -20,17 +21,66 @@
     }
 #endif
 
-void printCard(Card c) {
-    if (c.rank == -1) printf("[Empty]");
-    else printf("[%s of %s]", RANKS[c.rank], SUITS[c.suit]);
+// get the correct suit symbol based on OS
+const char* getSuitSymbol(int suitIdx) {
+#ifdef _WIN32
+    // Windows
+    const char *suits[] = {"H", "D", "C", "S"};
+#else
+    // Mac/Linux
+    const char *suits[] = {"♥", "♦", "♣", "♠"};
+#endif
+    return suits[suitIdx];
 }
 
-void printHand(Card hand[]) {
-    for (int i = 0; i < HAND_SIZE; i++) {
-        if (hand[i].rank != -1) {
-            printf("%d:", i + 1); printCard(hand[i]); printf("  ");
-        }
+// Renders a single played card as ASCII art
+void printCard(Card c) {
+    if (c.rank == -1) {
+        printf("[Empty]\n");
+        return;
     }
+
+    printf("\n");
+    printf("   +-----+\n");
+    printf("   |%-2s   |\n", RANKS[c.rank]);
+    printf("   |  %s  |\n", getSuitSymbol(c.suit));
+    printf("   |   %2s|\n", RANKS[c.rank]);
+    printf("   +-----+\n");
+}
+
+// Renders the entire hand side-by-side
+void printHand(Card hand[]) {
+    printf("\n");
+
+    // Row 0: Top border & Choice numbers
+    for (int i = 0; i < HAND_SIZE; i++) {
+        if (hand[i].rank != -1) printf("  %d: +-----+ ", i + 1);
+    }
+    printf("\n");
+
+    // Row 1: Top-left Rank
+    for (int i = 0; i < HAND_SIZE; i++) {
+        if (hand[i].rank != -1) printf("     |%-2s   | ", RANKS[hand[i].rank]);
+    }
+    printf("\n");
+
+    // Row 2: Center Suit
+    for (int i = 0; i < HAND_SIZE; i++) {
+        if (hand[i].rank != -1) printf("     |  %s  | ", getSuitSymbol(hand[i].suit));
+    }
+    printf("\n");
+
+    // Row 3: Bottom-right Rank
+    for (int i = 0; i < HAND_SIZE; i++) {
+        if (hand[i].rank != -1) printf("     |   %2s| ", RANKS[hand[i].rank]);
+    }
+    printf("\n");
+
+    // Row 4: Bottom border
+    for (int i = 0; i < HAND_SIZE; i++) {
+        if (hand[i].rank != -1) printf("     +-----+ ");
+    }
+    printf("\n");
 }
 
 void clearScreen() {
