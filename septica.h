7c51eb85cf9adb624f2b5df90f9bfdd678bc525b
DenisGhijan <denis-mihail-adrian.ghijan@student.upt.ro>
2026-03-29 10:51:50 +0300

    Update septica.h

--- septica.h
+++ septica.h
@@ -1,23 +1,20 @@
-#ifndef GAME_H
-#define GAME_H
+#ifndef SEPTICA_H
+#define SEPTICA_H
 
 #define DECK_SIZE 32
 #define HAND_SIZE 4
-#define PLAYERS 3
+#define MAX_PLAYERS 4
 
 typedef struct {
     int rank;
     int suit;
 } Card;
 
-// Global constants
 extern const char *RANKS[];
 extern const char *SUITS[];
-extern const char *NAMES[];
 
-// Core mechanics
 int getPoints(Card c);
 int isCut(Card c, int originalRank);
 void shuffle(Card *deck);
 
-#endif // GAME_H
+#endif // SEPTICA_H
