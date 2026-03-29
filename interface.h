04186b2b4fde996f877dcf321f3ffe991e8459e3
DenisGhijan <denis-mihail-adrian.ghijan@student.upt.ro>
2026-03-29 10:51:22 +0300

    Update interface.h
    Added function declarations for delayCPU and displayDeadCards.

--- interface.h
+++ interface.h
@@ -3,11 +3,16 @@
 
 #include "septica.h"
 
+const char* getPlayerName(int id, int isTeamMode);
 void printCard(Card c);
 void printHand(Card hand[]);
 void clearScreen();
 int readSingleDigit();
 void waitForAnyKey();
 void displayRules();
+void delayCPU(int milliseconds);
+
+// Carti jucate anterior
+void displayDeadCards(Card playedCards[MAX_PLAYERS][DECK_SIZE], int playedCount[MAX_PLAYERS], int difficulty, int isTeamMode, int numPlayers);
 
 #endif // INTERFACE_H
