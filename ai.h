624457c3a419093cddd265f167e643043014624d
DenisGhijan <denis-mihail-adrian.ghijan@student.upt.ro>
2026-03-29 10:50:24 +0300

    Update ai.h

--- ai.h
+++ ai.h
@@ -1,8 +1,8 @@
 #ifndef AI_H
 #define AI_H
- 
+
 #include "septica.h"
 
-int chooseCardAI(Card hand[], int originalRank, int trickPoints, int isLeader);
+int chooseCardAI(Card hand[], int originalRank, int trickPoints, int isLeader, int currentWinner, int myID, int isTeamMode);
 
 #endif // AI_H
