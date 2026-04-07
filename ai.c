ab6b151ece9c274e86a1de494791992826ac5ad9
DenisGhijan <denis-mihail-adrian.ghijan@student.upt.ro>
2026-04-07 11:20:11 +0300

    Update ai.c

--- ai.c
+++ ai.c
@@ -1,13 +1,14 @@
 #include "ai.h"
 
-int chooseCardAI(Card hand[], int originalRank, int trickPoints, int isLeader, int currentWinner, int myID, int isTeamMode) {
+int chooseCardAI(Card hand[], int originalRank, int trickPoints, int isLeader, int currentWinner, int myID, int numPlayers) {
     int hasMatch = -1, hasSeven = -1, hasTrash = -1, hasPoints = -1, fallback = -1;
+    int isTeamMode = (numPlayers == 4);
 
     for (int i = 0; i < HAND_SIZE; i++) {
         if (hand[i].rank != -1) {
             fallback = i;
             if (hand[i].rank == originalRank) hasMatch = i;
-            else if (hand[i].rank == 0) hasSeven = i;
+            else if (hand[i].rank == 0 || (numPlayers == 3 && hand[i].rank == 1)) hasSeven = i;
             else if (hand[i].rank == 3 || hand[i].rank == 7) hasPoints = i;
             else hasTrash = i;
         }
@@ -18,7 +19,6 @@ int chooseCardAI(Card hand[], int originalRank, int trickPoints, int isLeader, i
         return fallback;
     }
 
-    // 2v2 logic
     int isPartnerWinning = (isTeamMode && (currentWinner % 2 == myID % 2));
 
     if (isPartnerWinning) {
