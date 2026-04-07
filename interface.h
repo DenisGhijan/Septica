fbd170a6af411ac144487d0b111dea576f5b7fa4
DenisGhijan <denis-mihail-adrian.ghijan@student.upt.ro>
2026-04-07 11:20:47 +0300

    Update interface.h

--- interface.h
+++ interface.h
@@ -3,16 +3,21 @@
 
 #include "septica.h"
 
-const char* getPlayerName(int id, int isTeamMode);
+const char* getPlayerName(int id, int numHumans, int numPlayers);
+void promptNextPlayer(int id, int numHumans, int numPlayers);
 void printCard(Card c);
 void printHand(Card hand[]);
 void clearScreen();
+void flushInput();
 int readSingleDigit();
 void waitForAnyKey();
 void displayRules();
 void delayCPU(int milliseconds);
+void displayDeadCards(Card playedCards[MAX_PLAYERS][MAX_DECK_SIZE], int playedCount[MAX_PLAYERS], int difficulty, int numHumans, int numPlayers);
 
-// Carti jucate anterior
-void displayDeadCards(Card playedCards[MAX_PLAYERS][DECK_SIZE], int playedCount[MAX_PLAYERS], int difficulty, int isTeamMode, int numPlayers);
+// Helpers pentru printarea textelor colorate
+const char* getSuitSymbol(int suitIdx);
+const char* getSuitColor(int suitIdx);
+#define COLOR_RESET "\033[0m"
 
 #endif // INTERFACE_H
