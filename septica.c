be1a682f562bf5a0e3599855ecc7091fc2295cba
DenisGhijan <denis-mihail-adrian.ghijan@student.upt.ro>
2026-04-07 11:21:04 +0300

    Update septica.c

--- septica.c
+++ septica.c
@@ -8,13 +8,16 @@ int getPoints(Card c) {
     return (c.rank == 3 || c.rank == 7) ? 1 : 0;
 }
 
-int isCut(Card c, int originalRank) {
-    return (c.rank == originalRank || c.rank == 0);
+int isCut(Card c, int originalRank, int numPlayers) {
+    if (c.rank == originalRank || c.rank == 0) return 1;
+    // În 3 jucători, cei 8 (indexul 1) taie și ei!
+    if (numPlayers == 3 && c.rank == 1) return 1;
+    return 0;
 }
 
-void shuffle(Card *deck) {
-    for (int i = 0; i < DECK_SIZE; i++) {
-        int j = i + rand() / (RAND_MAX / (DECK_SIZE - i) + 1);
+void shuffle(Card *deck, int size) {
+    for (int i = 0; i < size; i++) {
+        int j = i + rand() / (RAND_MAX / (size - i) + 1);
         Card t = deck[j]; deck[j] = deck[i]; deck[i] = t;
     }
 }
