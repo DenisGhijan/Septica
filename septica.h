5b1a7ba3f88206c33a8ede92a8d03f30b0cb149d
DenisGhijan <denis-mihail-adrian.ghijan@student.upt.ro>
2026-04-07 11:21:15 +0300

    Update septica.h

--- septica.h
+++ septica.h
@@ -1,7 +1,7 @@
 #ifndef SEPTICA_H
 #define SEPTICA_H
 
-#define DECK_SIZE 32
+#define MAX_DECK_SIZE 32
 #define HAND_SIZE 4
 #define MAX_PLAYERS 4
 
@@ -14,7 +14,7 @@ extern const char *RANKS[];
 extern const char *SUITS[];
 
 int getPoints(Card c);
-int isCut(Card c, int originalRank);
-void shuffle(Card *deck);
+int isCut(Card c, int originalRank, int numPlayers);
+void shuffle(Card *deck, int size);
 
 #endif // SEPTICA_H
